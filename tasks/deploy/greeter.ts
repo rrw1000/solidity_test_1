5368731c587a9964a0b9f44b0e2cde31d6bd1fa4
Richard Watts <richard@bigpayme.com>
2022-04-06 20:17:32 +0100

    Revert "refactor: set signer deployer in deploy task"
    This reverts commit c45b1600eed1d5f5f72074d3050a165af6587a3d.

--- tasks/deploy/greeter.ts
+++ tasks/deploy/greeter.ts
@@ -1,4 +1,3 @@
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { task } from "hardhat/config";
 import { TaskArguments } from "hardhat/types";
 
@@ -8,9 +7,8 @@ import { Greeter__factory } from "../../src/types/factories/Greeter__factory";
 task("deploy:Greeter")
   .addParam("greeting", "Say hello, be nice")
   .setAction(async function (taskArguments: TaskArguments, { ethers }) {
-    const signers: SignerWithAddress[] = await ethers.getSigners();
     const greeterFactory: Greeter__factory = <Greeter__factory>await ethers.getContractFactory("Greeter");
-    const greeter: Greeter = <Greeter>await greeterFactory.deploy(taskArguments.greeting, { from: signers[0].address });
+    const greeter: Greeter = <Greeter>await greeterFactory.deploy(taskArguments.greeting);
     await greeter.deployed();
     console.log("Greeter deployed to: ", greeter.address);
   });
